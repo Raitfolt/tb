7e596be8383e4d5988b1d3ac7fd71c3943bad1c8
Антон Новосельцев <raitfolt@gmail.com>
2023-07-25 15:59:50 +0500

    tg events done

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,6 +1,8 @@
 package telegram
 
 import (
+	"errors"
+
 	"github.com/Raitfolt/tb/clients/telegram"
 	"github.com/Raitfolt/tb/events"
 	"github.com/Raitfolt/tb/lib/e"
@@ -17,6 +19,9 @@ type Meta struct {
 	ChatID   int
 	Username string
 }
+
+var ErrUnknownEventType = errors.New("unknown event type")
+var ErrUnknownMetaType = errors.New("unknown meta type")
 
 func New(client *telegram.Client, storage storage.Storage) *Processor {
 	return &Processor{
@@ -42,6 +47,37 @@ func (p *Processor) Fetch(limit int) ([]events.Event, error) {
 	}
 
 	p.offset = updates[len(updates)-1].ID + 1
+
+	return res, nil
+}
+
+func (p *Processor) Process(event events.Event) error {
+	switch event.Type {
+	case events.Message:
+		return p.processMessage(event)
+	default:
+		return e.Wrap("can't process message", ErrUnknownEventType)
+	}
+}
+
+func (p *Processor) processMessage(event events.Event) error {
+	meta, err := meta(event)
+	if err != nil {
+		return e.Wrap("can't process message", err)
+	}
+
+	if err := p.doCmd(event.Text, meta.ChatID, meta.Username); err != nil {
+		return e.Wrap("can't process message", err)
+	}
+
+	return nil
+}
+
+func meta(event events.Event) (Meta, error) {
+	res, ok := event.Meta.(Meta)
+	if !ok {
+		return Meta{}, e.Wrap("can't get meta", ErrUnknownMetaType)
+	}
 
 	return res, nil
 }
